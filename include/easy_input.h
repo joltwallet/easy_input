667791a83a1357891813cb694c08e53ad77e36e7
Brian Pugh <bnp117@gmail.com>
2018-04-22 22:28:37 -0700

    Fixed typedef buf

--- include/easy_input.h
+++ include/easy_input.h
@@ -1,17 +1,20 @@
 #ifndef __INCLUDE_EASY_INPUT_H__
 #define __INCLUDE_EASY_INPUT_H__
 
-    typedef enum buttons{
-        EASY_INPUT_UP,
-        EASY_INPUT_DOWN,
-        EASY_INPUT_LEFT,
-        EASY_INPUT_RIGHT,
-        EASY_INPUT_BACK,
-        EASY_INPUT_ENTER,
-        EASY_INPUT_NUMBER_OF_BUTTON_TYPES
-    } buttons_t;
+#include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
 
-    void easy_input_queue_init(QueueHandle_t *input_queue);
-    void easy_input_push_button_task( void *input_queue );
+typedef enum buttons{
+    EASY_INPUT_UP,
+    EASY_INPUT_DOWN,
+    EASY_INPUT_LEFT,
+    EASY_INPUT_RIGHT,
+    EASY_INPUT_BACK,
+    EASY_INPUT_ENTER,
+    EASY_INPUT_NUMBER_OF_BUTTON_TYPES
+} buttons_t;
+
+void easy_input_queue_init(QueueHandle_t *input_queue);
+void easy_input_push_button_task( void *input_queue );
 
 #endif
